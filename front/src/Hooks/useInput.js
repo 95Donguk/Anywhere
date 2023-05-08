793d3a999e5c1be8c7ca1a2ad8444b5ead7f1cf0
River <swimmingr@gmail.com>
2023-05-08 23:30:33 +0900

    add hooks useInput

---
+++ front/src/Hooks/useInput.js
@@ -0,0 +1,9 @@
+import { useState, useCallback } from "react";
+
+export default (initialState = null) => {
+  const [state, setState] = useState(initialState);
+  const handler = useCallback((e) => {
+    setState(e.target.value);
+  }, []);
+  return [state, handler, setState];
+};
