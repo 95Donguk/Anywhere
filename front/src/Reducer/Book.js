35d753d0dbfab1ed83c16efe5fee5b2f7bf833de
River <swimmingr@gmail.com>
2023-05-12 00:41:54 +0900

    add redux book

---
+++ front/src/Reducer/Book.js
@@ -0,0 +1,24 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    booking_no:0,
+    member_no:0,
+    room_no:0,
+    check_in:"",
+    check_out:"",
+    guests:0,
+    booking_status:0,
+    create_at:"",
+    updated_at:"",
+}
+
+const BookSlice = createSlice({
+    name:"book",
+    initialState,
+    reducers:{
+        BOOK_POST: (state, action) => {
+            state.push(action.payload);
+          },
+    }
+})
+export default BookSlice.reducer;
