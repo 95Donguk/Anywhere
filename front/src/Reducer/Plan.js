ae5fa26c781dc60960420d92332586af03586d0c
River <swimmingr@gmail.com>
2023-05-12 00:50:32 +0900

    add plan redux

---
+++ front/src/Reducer/Plan.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState ={
+    city_no:0,
+    room_no:0,
+    room_type_no:0,
+    host_member_no:0,
+    booking_no:0,
+    member_no:0,
+}
+
+const PlanSlice=createSlice({
+    name:"plan",
+    initialState,
+    reducers:{
+        PLAN_POST: (state, action) => {
+            state.push(action.payload);
+          },
+    }
+})
+
+export default PlanSlice;
+// 사용자가 선택한 사용자 정보,숙소.장소,날짜 저장
